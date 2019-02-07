91b938f6bc6716839d83a8098ea0c029306d29b9
Ricardo Marques <rimarques@suse.com>
2019-02-07 18:59:51 +0000

    Adds support for kernel lio backstore
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- ceph_iscsi_config/settings.py
+++ ceph_iscsi_config/settings.py
@@ -10,8 +10,8 @@ import hashlib
 import json
 import re
 
-from ceph_iscsi_config.gateway_setting import (TGT_SETTINGS, SYS_SETTINGS,
-                                               TCMU_SETTINGS)
+from ceph_iscsi_config.gateway_setting import (TGT_SETTINGS, TGT_KERNEL_SETTINGS, SYS_SETTINGS,
+                                               TCMU_SETTINGS, KERNEL_SETTINGS)
 
 
 # this module when imported preserves the global values
@@ -62,7 +62,9 @@ class Settings(object):
 
         self._add_attrs_from_defs(SYS_SETTINGS)
         self._add_attrs_from_defs(TGT_SETTINGS)
+        self._add_attrs_from_defs(TGT_KERNEL_SETTINGS)
         self._add_attrs_from_defs(TCMU_SETTINGS)
+        self._add_attrs_from_defs(KERNEL_SETTINGS)
 
         if len(dataset) != 0:
             # If we have a file use it to override the defaults
@@ -72,7 +74,9 @@ class Settings(object):
 
             if config.has_section("target"):
                 all_settings = TGT_SETTINGS.copy()
+                all_settings.update(TGT_KERNEL_SETTINGS)
                 all_settings.update(TCMU_SETTINGS)
+                all_settings.update(KERNEL_SETTINGS)
 
                 self._override_attrs_from_conf(config.items("target"),
                                                all_settings)
@@ -128,7 +132,9 @@ class Settings(object):
         sync_settings = {}
         self._hash_settings(SYS_SETTINGS.keys(), sync_settings)
         self._hash_settings(TGT_SETTINGS.keys(), sync_settings)
+        self._hash_settings(TGT_KERNEL_SETTINGS.keys(), sync_settings)
         self._hash_settings(TCMU_SETTINGS.keys(), sync_settings)
+        self._hash_settings(KERNEL_SETTINGS.keys(), sync_settings)
 
         h = hashlib.sha256()
         h.update(json.dumps(sync_settings).encode('utf-8'))
